48db9607d95e485e6dcc4a7304ebe887f27b5167
Thodoris Pliakas <thodorispliakas@gmail.com>
2017-04-01 21:15:03 +0300

    files

---
+++ angularFiles/controller.js
@@ -0,0 +1,11 @@
+
+app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {
+  forecast.success(function(data) {
+    $scope.fiveDay = data;
+
+  });
+      
+ 
+    
+   
+}]);
